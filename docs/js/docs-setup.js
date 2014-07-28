3d22a696f12d23880d88965d6d5fbd6f68b7b02a
Viktor Horváth <hovitya@outlook.com>
2014-07-28 23:24:12 +0200

    Add ngDoc

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,73 @@
+NG_DOCS={
+  "sections": {
+    "imxColorPicker": "IMX ColorPicker Documentation"
+  },
+  "pages": [
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker",
+      "shortName": "imx.colorPicker",
+      "type": "overview",
+      "moduleName": "imx.colorPicker",
+      "keywords": "colorpicker imx imxcolorpicker overview"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.directive:imxColorDisplay",
+      "shortName": "imxColorDisplay",
+      "type": "directive",
+      "moduleName": "imx.colorPicker",
+      "keywords": "color colorpicker directive ffffff formats hsl imx imxcolordisplay imxcolorpicker rgb string"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.directive:imxColorPicker",
+      "shortName": "imxColorPicker",
+      "type": "directive",
+      "moduleName": "imx.colorPicker",
+      "shortDescription": "Render color picker component.",
+      "keywords": "ae angular assignable base basecolors bind button call clicked closable close color colorpicker colors component current data data-bind directive display example expression format function hex html imx imxcolorpicker list mycolor ng-model ngmodel on-close output picker populated provided render selected service solid string template templateurl ui url"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.directive:imxColorShades",
+      "shortName": "imxColorShades",
+      "type": "directive",
+      "moduleName": "imx.colorPicker",
+      "shortDescription": "Render hue, brightness and saturation input control.",
+      "keywords": "angular assignable bind brightness colorpicker control data data-bind directive ea expression format hex hue imx imxcolorpicker input ngmodel render saturation service"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.directive:imxResize",
+      "shortName": "imxResize",
+      "type": "directive",
+      "moduleName": "imx.colorPicker",
+      "keywords": "callback check colorpicker directive frequency height imx imxcolorpicker imxresize imxtimeout parameters passed resize width"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.provider:imxColorProvider",
+      "shortName": "imxColorProvider",
+      "type": "interface",
+      "moduleName": "imx.colorPicker",
+      "keywords": "$new color colorpicker colorstr create ffffff format http imx imxcolorpicker instance interface method provider rgb string"
+    },
+    {
+      "section": "imxColorPicker",
+      "id": "imx.colorPicker.service:imxColorStoreService",
+      "shortName": "imxColorStoreService",
+      "type": "service",
+      "moduleName": "imx.colorPicker",
+      "keywords": "callback changed color colorpicker empty exists func function getpalette hascolor hex identifier imx imxcolorpicker limit method onpalettechanged palette paletteid prepend removecolor service signature size storecolor string strings updated"
+    }
+  ],
+  "apis": {
+    "imxColorPicker": true
+  },
+  "html5Mode": false,
+  "startPage": "docs",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
